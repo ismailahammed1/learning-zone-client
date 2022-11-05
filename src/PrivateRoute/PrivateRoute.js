fc0b2356e801ca4f10f997d685be135fd9f162a1
Md Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2022-11-06 02:39:35 +0600

    PrivateRoute setup

--- src/PrivateRoute/PrivateRoute.js
+++ src/PrivateRoute/PrivateRoute.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../component/Contexts/useContext";
 import LoadingSpinner from "../component/LoadingSpinner/LoadingSpinner";
-//route setup
+//PrivateRoute setup
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
